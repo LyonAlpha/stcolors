f485f190d13a0d315969ec1eb1b06c1eaeea5348
LyonAlpha <shivan.thaker@gmail.com>
2022-04-24 14:16:42 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# stcolors
- A Simple Easy-To-Use Color Scheme Module Program
+# A Easy To Use Color Module
+
+Get Perfect Color Schemes Every Time You Need Them!
+
+# Installation
+
+`npm i stcolors --save`
+
+Then...
+
+```
+import stcolors
+// stcolors.<color>
+```
+
+# Options
+
+StColors Supports Many Options, All Of Which Are Optional
+
+The Options Are:
+
+DarkRed
+LightRed
+
+Orange
+LightOrange
+
+Yellow
+LightYellow
+HazelYellow
+
+LightGreen
+DarkGreen
+Lime
+LightLime
+
+SeaGreen
+LightSeaGreen
+Teal
+Cyan
+WhiteCyan
+
+DarkBlue
+BasicBlue
+LightBlue
+
+Purple
+Violet
+LightViolet
+
+DarkPink
+Pink
+LightPink
+
+UltraSilver
+Silver
+Gray
+DarkGray
+BlackGray
+
+// Base Colors
+
+Black
+Red
+Green
+Blue
+White
---
+++ index.js
@@ -0,0 +1,101 @@
+function listColors() {
+    console.log(`
+DarkRed
+LightRed
+
+Orange
+LightOrange
+
+Yellow
+LightYellow
+HazelYellow
+
+LightGreen
+DarkGreen
+Lime
+LightLime
+
+SeaGreen
+LightSeaGreen
+Teal
+Cyan
+WhiteCyan
+
+DarkBlue
+BasicBlue
+LightBlue
+
+Purple
+Violet
+LightViolet
+
+DarkPink
+Pink
+LightPink
+
+UltraSilver
+Silver
+Gray
+DarkGray
+BlackGray
+
+// Base Colors
+
+Black
+Red
+Green
+Blue
+White`
+)}
+
+// Uncomment The Following Line If You Want To Print Out All The Colors To The Screen
+// listColors()
+
+darkred = 204, 0, 0
+lightred = 255, 51, 51
+
+orange = 255, 128, 0
+lightorange = 255, 153, 51
+
+yellow = 255, 255, 0
+lightyellow = 255, 255, 51
+hazelyellow = 255, 255, 102
+
+lightgreen = 51, 255, 51
+darkgreen = 0, 204, 0
+lime = 102, 204, 0
+lightlime = 128, 255, 0
+
+seagreen = 0, 204, 102
+lightseagreen = 0, 255, 128
+teal = 51, 255, 153
+cyan = 0, 255, 255
+whitecyan = 51, 255, 255
+
+darkblue = 0, 0, 153
+basicblue = 0, 0, 204
+lightblue = 51, 51, 255
+
+purple = 76, 0, 153
+violet = 102, 0, 102
+lightviolet = 153, 0, 153
+
+darkpink = 204, 0, 102
+pink = 255, 0, 127
+lightpink = 255, 51, 153
+
+ultrasilver = 244, 244, 244
+silver = 192, 192, 192
+gray = 96, 96, 96
+darkgray = 64, 64, 64
+blackgray = 32, 32, 32
+
+// BASE COLORS
+
+black = 0, 0, 0
+red = 255, 0, 0
+green = 0, 255, 0
+blue = 0, 0, 255
+white = 255, 255, 255
+
+module.exports.stcolors = stcolors;
